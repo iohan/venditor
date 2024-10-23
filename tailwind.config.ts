0f2be61fad3390133966221437a101d4af05235b
Johan Östling <johan.ostling@gmail.com>
2024-10-23 18:34:04 +0200

    Tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,8 @@ const config: Config = {
   theme: {
     container: {
       center: true,
+      padding: "1.5rem",
+      screens: { sm: "100%", md: "100%", lg: "100%", xl: "1200px" },
     },
     extend: {
       colors: {
