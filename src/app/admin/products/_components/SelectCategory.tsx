4a4442e11991a8ca39c1406f7b8552b01dfbfce0
Johan Östling <johan.ostling@gmail.com>
2024-11-01 22:42:36 +0100

    Update category

--- src/app/admin/products/_components/SelectCategory.tsx
+++ src/app/admin/products/_components/SelectCategory.tsx
@@ -3,12 +3,12 @@ import ContainerBox from "../../_components/ContainerBox";
 import { SquareX } from "lucide-react";
 import NewCategory from "./NewCategory";
 import { Category as CategoryType } from "@prisma/client";
-import { Product } from "./ProductForm";
+import { ProductType } from "@/data-layer/product";
 
 interface CategoryProps {
   categories: CategoryType[];
-  product: Product;
-  setProduct: (product: Product) => void;
+  product: ProductType;
+  setProduct: (product: ProductType) => void;
 }
 
 const Category = ({ categories, product, setProduct }: CategoryProps) => {
@@ -18,10 +18,13 @@ const Category = ({ categories, product, setProduct }: CategoryProps) => {
     const id = Number(category.value);
     const title = category.label;
 
-    if (!selectedCategories.some((c) => c.id === id)) {
+    if (!selectedCategories?.some((c) => c.id === id)) {
       setProduct({
         ...product,
-        selectedCategories: [...product.selectedCategories, { id, title }],
+        selectedCategories: [
+          ...(product.selectedCategories || []),
+          { id, title },
+        ],
       });
     }
   };
@@ -29,7 +32,7 @@ const Category = ({ categories, product, setProduct }: CategoryProps) => {
   const onUnselectCategory = (id: number | undefined, title: string) => {
     setProduct({
       ...product,
-      selectedCategories: selectedCategories.filter(
+      selectedCategories: selectedCategories?.filter(
         (selected) => selected.id !== id || selected.title !== title,
       ),
     });
@@ -40,7 +43,7 @@ const Category = ({ categories, product, setProduct }: CategoryProps) => {
       setProduct({
         ...product,
         selectedCategories: [
-          ...product.selectedCategories,
+          ...(product.selectedCategories || []),
           { id: undefined, title: newCategory },
         ],
       });
@@ -52,7 +55,7 @@ const Category = ({ categories, product, setProduct }: CategoryProps) => {
       <div className="font-semibold text-lg">Category</div>
       <Dropdown
         options={categories
-          .filter((c) => !selectedCategories.some((s) => s.id === c.id))
+          .filter((c) => !selectedCategories?.some((s) => s.id === c.id))
           .map((category) => ({
             label: category.title,
             value: String(category.id),
@@ -60,7 +63,7 @@ const Category = ({ categories, product, setProduct }: CategoryProps) => {
         onChange={onSelectCategory}
       />
       <div className="flex text-sm gap-2 flex-wrap">
-        {selectedCategories.map((s, i) => (
+        {selectedCategories?.map((s, i) => (
           <div
             key={s.id ? s.id : `unsaved_${i}`}
             className="flex-shrink-0 flex-grow-0 basis-auto"
